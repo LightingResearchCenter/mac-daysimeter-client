108c315e9bab87798061be34dfddf5a83a271354
Geoffrey Jones <jonesg5@rpi.edu>
2016-03-11 16:02:30 -0500

    Create OS X project

---
+++ Daysimeter Client/Daysimeter Client/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Daysimeter Client
+//
+//  Created by Geoffrey Jones on 3/11/16.
+//  Copyright © 2016 Rensselaer Polytechnic Institute, Lighting Research Center. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
